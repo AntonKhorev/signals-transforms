3c146f1c7ed0710c86c12b6cfbc08ae936d231bf
Anton Khorev <tony29@yandex.ru>
2015-05-15 06:38:31 +0300

    dropdown changes caption

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 $(function(){
+	var transforms=[
+		'Discrete-time Fourier transform (DTFT)',
+		'Discrete Fourier transform (DFT)'
+	];
 	$('.signal-transform-properties').each(function(){
-		var transform=$('caption');
-		var transformSelect=null;
-		transform.click(function(){
-			if (transformSelect===null) {
-				transformSelect=$("<ul class='transform-select'>"+
-					"<li>Discrete-time Fourier transform (DTFT)</li>"+
-					"<li>Discrete Fourier transform (DFT)</li>"+
-				"</ul>");
-				transform.append(transformSelect);
+		var transformElm=$('caption'); // TODO check if it's relative to the table
+		var transformSelectElm=null;
+		transformElm.wrapInner("<span class='signal-transform-dropdown'>");
+		var transformDropdownElm=transformElm.find('.signal-transform-dropdown');
+		transformDropdownElm.click(function(){
+			if (transformSelectElm===null) {
+				transformSelectElm=$("<ul class='transform-select' />");
+				for (var i=0;i<transforms.length;i++) {
+					transformSelectElm.append(
+						$("<li>"+transforms[i]+"</li>").click(transforms[i],function(e){
+							transformDropdownElm.text(e.data);
+							transformSelectElm.remove();
+							transformSelectElm=null;
+						})
+					);
+				}
+				transformElm.append(transformSelectElm);
 			} else {
-				transformSelect.remove();
-				transformSelect=null;
+				transformSelectElm.remove();
+				transformSelectElm=null;
 			}
 		});
 	});
