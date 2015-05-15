3800bb621590dff8e80be03482d9127547107d61
Anton Khorev <tony29@yandex.ru>
2015-05-15 08:40:34 +0300

    transform definitions

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ $(function(){
 	var transforms=[
 		{
 			name:'Discrete-time Fourier transform (DTFT)',
+			timeDefinition:'x[n] = \\frac{1}{2\\pi} \\int \\limits_{\\langle 2\\pi \\rangle} X(e^{j\\omega}) e^{j\\omega n} \\, \\mathrm{d}\\omega',
+			freqDefinition:'X(e^{j\\omega}) = \\sum_{n=-\\infty}^{+\\infty} x[n] e^{-j\\omega n}',
 			timeFormula:function(neg,inv,conj){
 				return (neg?'-':'')+'x'+(conj?'^*':'')+'['+(inv?'-':'')+'n]';
 			},
@@ -9,6 +11,8 @@ $(function(){
 				return (neg?'-':'')+'X'+(conj?'^*':'')+'(e^{'+(inv?'-':'')+'j\\omega})';
 			}
 		},{
+			timeDefinition:'x[n] = \\frac 1 N \\sum_{k=0}^{N-1} X[k] W_N^{-kn}',
+			freqDefinition:'X[k] = \\sum_{k=0}^{N-1} x[n] W_N^{kn}',
 			name:'Discrete Fourier transform (DFT)',
 			timeFormula:function(neg,inv,conj){
 				return (neg?'-':'')+'x'+(conj?'^*':'')+'['+(inv?'-':'')+'n]';
@@ -43,7 +47,16 @@ $(function(){
 							transformSelectElm.remove();
 							transformSelectElm=null;
 							transformDropdownElm.text(transform.name);
-							tableElm.find('tbody').children('tr').each(function(i){
+							tableElm.find('.signal-transform-properties-definition').children('tr').children('td').text(function(j){
+								if (j==0) {
+									return '$$'+transform.timeDefinition+'$$';
+								} else if (j==2) {
+									return '$$'+transform.freqDefinition+'$$';
+								} else {
+									return '';
+								}
+							});
+							tableElm.find('.signal-transform-properties-conjinv').children('tr').each(function(i){
 								$(this).children('td').text(function(j){
 									var pattern,formula;
 									if (j<3) {
