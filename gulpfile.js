76801f1ef223bc731558f88f04116c963f5e524b
Anton Khorev <tony29@yandex.ru>
2015-05-21 16:58:19 +0300

    autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp=require('gulp');
 var less=require('gulp-less');
+var autoprefixer=require('gulp-autoprefixer');
 
 gulp.task('index.css',function(){
 	gulp.src('src/index.less')
 		.pipe(less())
+		.pipe(autoprefixer())
 		.pipe(gulp.dest('public_html'));
 });
 
