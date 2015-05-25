1e6ff1478490c8d0cc3461ec36b10151c039d4ed
Anton Khorev <tony29@yandex.ru>
2015-05-26 01:45:04 +0300

    gulp watch js

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,32 @@ var less=require('gulp-less');
 var autoprefixer=require('gulp-autoprefixer');
 var concat=require('gulp-concat');
 
+var cssSrc='src/signals-transforms-table.less';
+var jsSrc=[
+	'src/intro.js',
+	'src/helpers.js',
+	'src/sections.js',
+	'src/transforms.js',
+	'src/main.js',
+	'src/outro.js'
+];
+
 gulp.task('css',function(){
-	gulp.src('src/signals-transforms-table.less')
+	gulp.src(cssSrc)
 		.pipe(less())
 		.pipe(autoprefixer())
 		.pipe(gulp.dest('public_html'));
 });
 
 gulp.task('js',function(){
-	gulp.src([
-		'src/intro.js',
-		'src/helpers.js',
-		'src/sections.js',
-		'src/transforms.js',
-		'src/main.js',
-		'src/outro.js'
-	])
+	gulp.src(jsSrc)
 		.pipe(concat('signals-transforms-table.js'))
 		.pipe(gulp.dest('public_html'));
 });
 
 gulp.task('watch',function(){
-	gulp.watch('src/signals-transforms-table.less',['css']);
+	gulp.watch(cssSrc,['css']);
+	gulp.watch(jsSrc,['js']);
 });
 
 gulp.task('default',['css','js']);
